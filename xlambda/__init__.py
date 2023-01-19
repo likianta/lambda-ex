f3c22d960b580c4ce2138ae1553d9350354c3b98
likianta <likianta@foxmail.com>
2023-01-19 17:50:44 +0800

    rename project (test stage)

---
+++ xlambda/__init__.py
@@ -0,0 +1,10 @@
+from .grafting import GRAFTEE
+from .grafting import grafting
+from .grafting import grafting as bind
+from .grafting import hold
+from .grafting import partial
+from .lambdex import run_once
+from .lambdex import xlambda
+from .later import later
+
+__version__ = '2.0.0'
